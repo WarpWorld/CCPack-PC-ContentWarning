7b1295793f36c1fb06dfcd58319fdae6ceee0887
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2025-09-16 20:43:00 +0000

    Crowd Control Effect Pack Synchronization Tool - Automated Commit

--- ContentWarning.cs
+++ ContentWarning.cs
@@ -1,4 +1,5 @@
-﻿using ConnectorLib.SimpleTCP;
+﻿using System.Diagnostics.CodeAnalysis;
+using ConnectorLib.SimpleTCP;
 using CrowdControl.Common;
 using ConnectorType = CrowdControl.Common.ConnectorType;
 
@@ -10,6 +11,7 @@ public class ContentWarning : SimpleTCPPack<SimpleTCPServerConnector>
 
     public override ushort Port => 51337;
 
+    [SuppressMessage("PackMetadata", "CC1007:Message Format Property")]
     public override ISimpleTCPPack.MessageFormatType MessageFormat => ISimpleTCPPack.MessageFormatType.CrowdControlLegacy;
 
     public ContentWarning(UserRecord player, Func<CrowdControlBlock, bool> responseHandler, Action<object> statusUpdateHandler) : base(player, responseHandler, statusUpdateHandler) { }
